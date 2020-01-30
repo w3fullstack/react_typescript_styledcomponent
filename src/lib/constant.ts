d86030e40790bfe013150085fd4a88436e6f91b5
unknown <brightday1020@gmail.com>
2020-01-30 11:27:16 +0800

    update

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,6 +1,6 @@
 export const ownerMenuItems: User[] = [
-  {id: 1, avatar: "https://www.upwork.com/profile-portraits/c1ka3F3gwAqdHua2faa3lJEL0c0NX5envdwLKi4-KnwirWoEqBWkoacN5f8PUe6biI", name: "Ross Rich", role: "Manager"},
-  {id: 2, avatar: "https://www.upwork.com/profile-portraits/c1ka3F3gwAqdHua2faa3lJEL0c0NX5envdwLKi4-KnwirWoEqBWkoacN5f8PUe6biI", name: "Harry Avery", role: "Associate"},
+  {id: 1, avatar: "https://www.upwork.com/profile-portraits/c1cBqbtVvqBjdzMhHHKjoDG33XRoI5ZiAVBN7ArlhSdmAA1sulSyS5VpIlWEZG-jfM", name: "Ross Rich", role: "Manager"},
+  {id: 2, avatar: "https://www.upwork.com/profile-portraits/c1cBqbtVvqBjdzMhHHKjoDG33XRoI5ZiAVBN7ArlhSdmAA1sulSyS5VpIlWEZG-jfM", name: "Matias Capuano", role: "Associate"},
   {id: 3, avatar: "", name: "Amit Patel", role: "Associate"},
   {id: 4, avatar: "", name: "Suzy Anderson", role: "Associate"}
 ];
